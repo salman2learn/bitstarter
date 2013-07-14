26d675b6abf4ca67b4ea8fedb494f1ed01877d3e
Salman 2 learn <Salman2learn@gmail.com>
2013-07-14 01:09:14 +0000

    web js reads index 2

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFileSync('index.html', function (err, data) {
 	if (err) throw err;
-	response.send(data);
+	
+	response.send(data.toString());
   });
   response.send('Hello World');
 });
